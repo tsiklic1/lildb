18ffd77aa8652f0944d81e4391ca33f1df04703c
tsiklic1 <toma.siklic@gmail.com>
2026-06-25 15:21:04 +0200

    prepare parsing

--- crates/repl/src/main.rs
+++ crates/repl/src/main.rs
@@ -1,10 +1,17 @@
-use std::io::{self, BufRead, Write};
+use std::{
+    io::{self, BufRead, Write},
+    os::macos::raw::stat,
+};
+
+use anyhow::Error;
+
+use crate::StatementType::Insert;
 
 fn main() {
     let stdin = io::stdin();
 
     loop {
-        print!("db > ");
+        print!("lildb > ");
         io::stdout().flush().unwrap();
 
         let mut command = String::new();
@@ -12,9 +19,54 @@ fn main() {
 
         let command = command.trim();
 
-        match command {
-            ".exit" => break,
-            _ => println!("Unrecognized command: {}.", command),
+        match command.as_bytes()[0] {
+            b'.' => match command {
+                //meta command
+                ".exit" => break,
+                _ => println!("Unrecognized meta command: {}.", command),
+            },
+            //statement
+            _ => {
+                let prepare_result = prepare_statement(command);
+                match prepare_result {
+                    Ok(statement) => {
+                        execute_statement(statement);
+                    }
+                    Err(()) => println!("Unrecognized statement keyword: {}", command),
+                }
+            }
         }
     }
+}
+
+fn prepare_statement(input: &str) -> Result<Statement, ()> {
+    let mut statement = Statement {
+        statement_type: StatementType::Select,
+    };
+    if input.starts_with("insert") {
+        statement.statement_type = StatementType::Insert;
+        return Ok(statement);
+    }
+    if input.starts_with("select") {
+        statement.statement_type = StatementType::Select;
+        return Ok(statement);
+    }
+    println!("abc");
+    Err(())
+}
+
+fn execute_statement(statement: Statement) {
+    match statement.statement_type {
+        StatementType::Insert => println!("Here we will do the insert"),
+        StatementType::Select => println!("Here we will do the select"),
+    }
+}
+
+enum StatementType {
+    Insert,
+    Select,
+}
+
+struct Statement {
+    statement_type: StatementType,
 }
