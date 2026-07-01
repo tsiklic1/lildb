d4bc8324fccd918ec2671ae0d0fee3971150749a
tsiklic1 <toma.siklic@gmail.com>
2026-07-01 12:37:09 +0200

    Add initial persistance

--- crates/repl/src/main.rs
+++ crates/repl/src/main.rs
@@ -1,9 +1,16 @@
-use std::io::{self, BufRead, Write};
+use std::{
+    fs::{File, OpenOptions},
+    io::{self, BufRead, Read, Seek, Write},
+    process::exit,
+};
 
 fn main() {
     let stdin = io::stdin();
 
-    let mut table = new_table();
+    println!("Enter db filename");
+    let mut filename = String::new();
+    stdin.lock().read_line(&mut filename).unwrap();
+    let mut table = db_open(&filename.trim());
 
     loop {
         print!("lildb > ");
@@ -17,7 +24,10 @@ fn main() {
         match command.as_bytes()[0] {
             b'.' => match command {
                 //meta command
-                ".exit" => break,
+                ".exit" => {
+                    db_close(&mut table);
+                    break;
+                }
                 _ => println!("Unrecognized meta command: {}.", command),
             },
             //statement
@@ -47,13 +57,20 @@ fn prepare_statement(input: &str) -> Result<Statement, ()> {
         let mut parts = input.trim().split_whitespace();
         let _command = parts.next();
         let id = parts.next().ok_or(())?;
+
+        let parsed_id = id.parse::<i32>().map_err(|_| ())?;
+
+        if parsed_id < 0 {
+            return Err(());
+        }
+
         let username = parts.next().ok_or(())?;
         let email = parts.next().ok_or(())?;
 
         let statement = Statement {
             statement_type: StatementType::Insert,
             row_to_insert: Row {
-                id: id.parse::<u32>().map_err(|_| ())?,
+                id: parsed_id as u32,
                 username: fixed_bytes(username)?,
                 email: fixed_bytes(email)?,
             },
@@ -85,7 +102,7 @@ struct Row {
 
 struct Table {
     num_rows: u32,
-    pages: [Option<Box<[u8; PAGE_SIZE]>>; TABLE_MAX_PAGES],
+    pager: Pager,
 }
 
 const ID_SIZE: usize = 4;
@@ -102,11 +119,11 @@ const ROWS_PER_PAGE: usize = PAGE_SIZE / ROW_SIZE;
 
 fn row_slot(table: &mut Table, row_num: usize) -> &mut [u8] {
     let page_num = row_num / ROWS_PER_PAGE;
-    if table.pages[page_num].is_none() {
-        table.pages[page_num] = Some(Box::new([0; PAGE_SIZE]));
-    }
 
-    let page = table.pages[page_num].as_mut().unwrap();
+    let page = get_page(&mut table.pager, page_num).unwrap_or_else(|_| {
+        println!("Page doesn't exist");
+        exit(1);
+    });
 
     let row_offset = row_num % ROWS_PER_PAGE;
     let byte_offset = row_offset * ROW_SIZE as usize;
@@ -117,7 +134,9 @@ fn row_slot(table: &mut Table, row_num: usize) -> &mut [u8] {
 fn execute_statement(statement: Statement, table: &mut Table) {
     match statement.statement_type {
         StatementType::Insert => {
-            execute_insert(&statement, table);
+            if execute_insert(&statement, table).is_err() {
+                println!("Error: Table full.");
+            }
         }
         StatementType::Select => {
             execute_select(&statement, table);
@@ -182,9 +201,113 @@ fn execute_select(statement: &Statement, table: &mut Table) -> Result<(), ()> {
     Ok(())
 }
 
-fn new_table() -> Box<Table> {
-    Box::new(Table {
-        num_rows: 0,
-        pages: std::array::from_fn(|_| None),
+fn db_open(filename: &str) -> Box<Table> {
+    let pager = match pager_open(filename) {
+        Ok(pager) => pager,
+        Err(()) => {
+            println!("Unable to open pager");
+            exit(1);
+        }
+    };
+    let num_rows = pager.file_length / ROW_SIZE as u32;
+
+    let table = Table { num_rows, pager };
+
+    Box::new(table)
+}
+
+struct Pager {
+    file: File,
+    file_length: u32,
+    pages: [Option<Box<[u8; PAGE_SIZE]>>; TABLE_MAX_PAGES],
+}
+
+fn pager_open(filename: &str) -> Result<Pager, ()> {
+    let file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open(filename)
+        .map_err(|_| ())?;
+
+    let file_length = file.metadata().map_err(|_| ())?.len() as u32;
+
+    let pages = std::array::from_fn(|_| None);
+
+    Ok(Pager {
+        file,
+        file_length,
+        pages,
     })
+}
+
+fn get_page(pager: &mut Pager, page_num: usize) -> Result<&mut [u8; PAGE_SIZE], ()> {
+    if page_num >= TABLE_MAX_PAGES {
+        return Err(());
+    }
+
+    if pager.pages[page_num].is_none() {
+        //Cache miss. Allocate memory and load from file.
+        let mut page = Box::new([0; PAGE_SIZE]);
+        let mut num_pages = pager.file_length / PAGE_SIZE as u32;
+
+        // We might save a partial page at the end of the file
+        if pager.file_length % PAGE_SIZE as u32 != 0 {
+            num_pages += 1;
+        }
+
+        if page_num < num_pages as usize {
+            pager
+                .file
+                .seek(io::SeekFrom::Start((page_num * PAGE_SIZE) as u64))
+                .map_err(|_| ())?;
+
+            pager.file.read(&mut page[..]).map_err(|_| ())?;
+        }
+
+        pager.pages[page_num] = Some(page);
+    }
+
+    Ok(pager.pages[page_num].as_mut().unwrap())
+}
+
+fn db_close(table: &mut Table) {
+    let num_full_pages = table.num_rows / ROWS_PER_PAGE as u32;
+
+    for i in 0..num_full_pages {
+        if table.pager.pages[i as usize].is_some() {
+            pager_flush(&mut table.pager, i, PAGE_SIZE as u32);
+        }
+    }
+
+    let num_additional_rows = table.num_rows % ROWS_PER_PAGE as u32;
+    if num_additional_rows > 0 {
+        let page_num = num_full_pages;
+        if table.pager.pages[page_num as usize].is_some() {
+            pager_flush(
+                &mut table.pager,
+                page_num,
+                num_additional_rows * ROW_SIZE as u32,
+            );
+        }
+    }
+}
+
+fn pager_flush(pager: &mut Pager, page_num: u32, size: u32) -> Result<(), ()> {
+    if pager.pages[page_num as usize].is_none() {
+        println!("Tried to flush null page");
+        return Err(());
+    };
+
+    pager
+        .file
+        .seek(io::SeekFrom::Start(page_num as u64 * PAGE_SIZE as u64))
+        .map_err(|_| ())?;
+
+    pager
+        .file
+        .write_all(&pager.pages[page_num as usize].as_ref().unwrap()[..size as usize])
+        .map_err(|_| ())?;
+
+    Ok(())
 }
